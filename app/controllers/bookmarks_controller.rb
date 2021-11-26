3d869774270f64e0e7f77ea767e48b2c558afc97
Stephen Kusrianto <stephenkusrianto@gmail.com>
2021-11-26 13:58:24 +0800

    made changes to file

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,59 +1,34 @@
 class BookmarksController < ApplicationController
-    before_action :set_bookmark, only: [:show, :edit, :update, :destroy]
+  before_action :set_bookmark, only: :destroy
+  before_action :set_list, only: [:new, :create]
 
-  # GET /restaurants
-  def index
-    @bookmarks = Bookmark.all
-  end
-
-  # GET /restaurants/1
-  def show
-  end
-
-  # GET /restaurants/new
   def new
-    @bookmark = bookmark.new
+    @bookmark = Bookmark.new
   end
 
-  # GET /restaurants/1/edit
-  def edit
-  end
-
-  # POST /restaurants
   def create
-    @bookmark = bookmark.new(bookmark_params)
-
-    if @bookmark.save
-      redirect_to @bookmark, notice: 'bookmark was successfully added.'
-    else
-      render :new
-    end
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    flash[:notice] = @bookmark.errors.full_messages.to_sentence unless @bookmark.save
+    redirect_to list_path(@list)
   end
 
-  # PATCH/PUT /restaurants/1
-  def update
-    if @bookmark.update(bookmark_params)
-      redirect_to @bookmark, notice: 'bookmark was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  # DELETE /restaurants/1
   def destroy
     @bookmark.destroy
-    redirect_to bookmarks_url, notice: 'bookmark was successfully removed.'
+    redirect_to list_path(@bookmark.list)
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_bookmark
-    @bookmark = bookmark.find(params[:id])
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 
-  # Only allow a list of trusted parameters through.
-  def list_params
-    params.require(:list).permit(:name)
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
   end
 end
